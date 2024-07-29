e8734025046d379b216051ca0f33ecda77658b28
John Kyle Lampa <jklampa31@gmail.com>
2024-07-30 01:45:19 +0800

    fix: added font family to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        poppins: ['var(--font-poppins)'],
+      }
     },
     colors: {
       darkblue: 'rgb(var(--color-dark-blue) / <alpha-value>)',
