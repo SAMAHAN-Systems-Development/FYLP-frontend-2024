49a1d41996c62999a3c6bb7191c7b3ce02a264d7
John Kyle Lampa <jklampa31@gmail.com>
2024-07-30 17:03:51 +0800

    fix: added font families in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,17 @@
+import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
+ import { Inter, Poppins,} from 'next/font/google';
+
+ export const inter = Inter({
+     subsets: ['latin'],
+     variable: '--font-inter',
+ });
+
+ export const poppins = Poppins({
+     weight: '400',
+     subsets: ['latin'],
+     variable: '--font-poppins',
+ });
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -8,6 +22,13 @@ const preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <div className={`--font-poppins ${poppins.variable}`}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
