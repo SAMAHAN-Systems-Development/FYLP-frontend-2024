963d91880a844dc9e9917ee14e22d12cc0230536
Miraii133 <jiyovalmoria@gmail.com>
2024-08-05 02:02:50 +0800

    feat: working check status role button UI and component.

---
+++ src/components/CheckStatusComponent.js
@@ -0,0 +1,28 @@
+'use client';
+import React, { useState } from 'react';
+import CheckStatusButton from '@/components/ui/CheckStatusButton';
+
+const ButtonGroup = () => {
+  const [selectedRole, setSelectedRole] = useState('');
+
+  const handleButtonClick = (e) => {
+    setSelectedRole(e.target.value);
+  };
+
+  return (
+    <div>
+      <CheckStatusButton
+        text={'DELEGATE'}
+        handleButtonClick={handleButtonClick}
+        isSelected={selectedRole === 'DELEGATE'}
+      />
+      <CheckStatusButton
+        text={'MENTOR'}
+        handleButtonClick={handleButtonClick}
+        isSelected={selectedRole === 'MENTOR'}
+      />
+    </div>
+  );
+};
+
+export default ButtonGroup;
