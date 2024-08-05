36c2b7306a3589c44ec48e3769f160d5ab0a44df
unknown <paololuisramirez@gmail.com>
2024-08-05 14:59:36 +0800

    feat: added link to section 5 for registration, fixed page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 import { Metadata } from 'next';
-import NavigationBar from '../components/ui/NavigationBar';
 import HeroSection from '../components/Sections/HeroSection';
 import Section2 from '../components/Sections/Section2';
-import CheckStatusTxtField from '../components/CheckStatusTxtField'
+import Section3 from '../components/Sections/Section3';
+import SectionFour from '../components/Sections/SectionFour';
+import SectionFive from '../components/Sections/SectionFive';
 
 export const metadata = {
   title: 'FYLP: Filipino Youth Leadership Program 2024',
@@ -13,15 +14,12 @@ export default function Home() {
   return (
     <main>
       {/* add sections here */}
-      {/* <NavigationBar />
       <HeroSection />
-      <Section2 /> */}
-      <CheckStatusTxtField />
-
-//       <Section2 />
-//       <Section3 />
-//       <SectionFour />
-//       <SectionFive />
+      <Section2 />
+      <Section2 />
+      <Section3 />
+      <SectionFour />
+      <SectionFive />
       {/* FYLP: Filipino Youth Leadership Program 2024 */}
     </main>
   );
