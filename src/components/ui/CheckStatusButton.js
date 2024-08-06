bec9ef5e67ffca54670575975405024af89a3689
Rhaey <rhaeyislyfe@gmail.com>
2024-08-07 07:57:08 +0800

    feat: integrate check status

--- src/components/ui/CheckStatusButton.js
+++ src/components/ui/CheckStatusButton.js
@@ -1,21 +1,15 @@
 'use client';
 import React, { useState } from 'react';
 
-const CheckStatusButton = ({ text, handleButtonClick, isSelected }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
+const CheckStatusButton = ({ text, handleButtonClick, isSelected, name }) => {
   return (
     <button
-      className={`justify-center m-2 p-5 w-1/6 min-w-40 max-w-48 rounded-md border-2 border-[#6b7280] transition duration-300 ease-in-out
-        ${isHovered ? 'hover:border-[#FD1E0A] text-[#FD1E0A] font-semibold' : 'bg-orange'} }
+      name={name}
+      className={`justify-center m-2 p-5 w-1/6 min-w-40 max-w-48 rounded-md border-2 border-[#6b7280] transition duration-300 ease-in-out uppercase hover:border-[#FD1E0A] hover:text-[#FD1E0A] hover:font-semibold bg-orange' 
         ${isSelected ? 'bg-[#FD1E0A] text-white border-0 font-semibold' : ''} 
         `}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
       value={text}
-      onClick={(e) => {
-        handleButtonClick(e);
-      }}
+      onClick={handleButtonClick}
     >
       {text}
     </button>
