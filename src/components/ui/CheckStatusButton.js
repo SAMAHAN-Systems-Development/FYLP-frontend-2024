d886f0b1e825dc20459825c2d6632ff0fd5563b8
Miraii133 <jiyovalmoria@gmail.com>
2024-08-06 12:48:47 +0800

    fix: text on button not semi-bold once selected.

--- src/components/ui/CheckStatusButton.js
+++ src/components/ui/CheckStatusButton.js
@@ -8,7 +8,7 @@ const CheckStatusButton = ({ text, handleButtonClick, isSelected }) => {
     <button
       className={`justify-center m-2 p-5 w-1/6 min-w-40 max-w-48 rounded-md border-2 border-[#6b7280] transition duration-300 ease-in-out
         ${isHovered ? 'hover:border-[#FD1E0A] text-[#FD1E0A] font-semibold' : 'bg-orange'} }
-        ${isSelected ? 'bg-[#FD1E0A] text-white border-0' : ''} 
+        ${isSelected ? 'bg-[#FD1E0A] text-white border-0 font-semibold' : ''} 
         `}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
