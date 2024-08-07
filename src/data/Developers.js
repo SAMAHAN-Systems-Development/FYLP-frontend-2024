11a13c916b266ebf1b8bf7f06201c24b0466f0f2
John Kyle Lampa <jklampa31@gmail.com>
2024-08-07 22:13:00 +0800

    feat(#38): added the meet the devs page

---
+++ src/data/Developers.js
@@ -0,0 +1,45 @@
+import projectManager from '../../public/assets/images/developers/Jiyo.png';
+import backendHead from '../../public/assets/images/developers/Rhaey.png';
+import frontendHead from '../../public/assets/images/developers/Paolo.png';
+import uiuxHead from '../../public/assets/images/developers/Maverick.png';
+import frontendDeveloper1 from '../../public/assets/images/developers/Mariel.png'
+import frontendDeveloper2 from '../../public/assets/images/developers/John.png'
+import frontendDeveloper3 from '../../public/assets/images/developers/Kervi.png'
+
+export const DevelopersData = [
+    {
+        imageUrl: projectManager,
+        name: 'Jiyo Seth Valmoria',
+        position: 'Project Manager',
+    },
+    {
+        imageUrl: backendHead,
+        name: 'Francis Rhaey Casas',
+        position: 'Backend Head',
+    },
+    {
+        imageUrl: frontendHead,
+        name: 'Paolo Luis Ramirez',
+        position: 'Frontend Head',
+    },
+    {
+        imageUrl: uiuxHead,
+        name: 'Maverick Jasper Pigao',
+        position: 'UI/UX Head',
+    },
+    {
+        imageUrl: frontendDeveloper1,
+        name: 'Mariel Mabano',
+        position: 'Frontend Developer',
+    },
+    {
+        imageUrl: frontendDeveloper2,
+        name: 'John Kyle Lampa',
+        position: 'Frontend Developer',
+    },
+    {
+        imageUrl: frontendDeveloper3,
+        name: 'Kervi Asombrado',
+        position: 'Frontend Developer',
+    }
+]
