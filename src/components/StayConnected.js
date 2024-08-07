0372f3d117ffcfc1ac0fe3bb984fcb3fd5edc062
unknown <paololuisramirez@gmail.com>
2024-08-07 11:51:57 +0800

    fix: adjusted fonts and margin in FAQ page and fixed the font for accordion

--- src/components/StayConnected.js
+++ src/components/StayConnected.js
@@ -4,10 +4,10 @@ import Socials from './ui/Socials';
 const StayConnected = () => {
   return (
     <div className="flex flex-col justify-center pt-[9.38rem] gap-6">
-      <h1 className="font-semibold text-xl text-center font-poppins">
+      <h1 className="font-semibold sm:text-xl text-[16.67px] text-center font-poppins">
         Stay Connected
       </h1>
-      <p className="text-center font-poppins">
+      <p className="text-center font-poppins sm:text-base text-[13.33px]">
         Follow us on social media to stay updated on the latest news and events
         related to the FYLP 2024!
       </p>
