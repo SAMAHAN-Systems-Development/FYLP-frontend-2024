10dfba514011021508e5050cb1ef9ab00c54d930
John Kyle Lampa <jklampa31@gmail.com>
2024-08-08 08:44:48 +0800

    feat: added paragraph in meet the devs

--- src/app/about-the-developers/page.js
+++ src/app/about-the-developers/page.js
@@ -6,11 +6,17 @@ import { DevelopersData } from '@/data/Developers';
 export default function Page() {
   return (
     <div className="pt-24 px-20 xsm:px-20 sm:pt-32 pb-24 flex flex-col items-center font-poppins">
-      <div className="px-15 xsm:px-18 text-center">
-        <p className="text-[18px] xsm:text-[20px] font-semibold pb-5">
-        Meet the Developers
+      <div className="grid gap-4 text-center">
+        <div className="px-15 xsm:px-18 text-center">
+          <p className="text-[18px] xsm:text-[20px] font-semibold pb-5">
+          Meet the Developers
+          </p>
+        </div>
+        <p className="text-center font-poppins lg:text-base xsm:text-[0.83331rem]">
+          Made possible by the  <span className="text-blue"> SAMAHAN Systems Development Team</span>
         </p>
       </div>
+      
       <div className="pt-20 grid lg:grid-cols-2 sm:lg-col-1 gap-16">
         {DevelopersData.map((item, index) => (
           <div
