2eaca1beecae0b9319bee6bb1c694fc87ba1f93f
Ivureku <insertmailherelol@gmail.com>
2024-08-09 22:43:44 +0800

    fix: status page button resposiveness adjusted

--- src/components/ui/CheckStatusButton.js
+++ src/components/ui/CheckStatusButton.js
@@ -6,7 +6,7 @@ const CheckStatusButton = ({ text, handleButtonClick, isSelected, name }) => {
   return (
     <button
       name={name}
-      className={`justify-center p-5 w-1/6 min-w-40 max-w-48 rounded-md transition duration-300 ease-in-out uppercase
+      className={`justify-center p-5 w-1/6 min-w-28 max-w-48 rounded-md transition duration-300 ease-in-out uppercase
         ${isHovered ? 'hover:border-[#FD1E0A] text-[#FD1E0A] font-semibold' : 'bg-orange'}
         ${isSelected ? 'bg-[#FD1E0A] text-white font-semibold border-0' : 'border-2 border-[#6b7280]'}
         `}
